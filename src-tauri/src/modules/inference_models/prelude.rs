d13db1c479dea3ce78c557a6e116f7b16d416e0c
Le An Nguyen <nguyenlean96@gmail.com>
2026-01-13 22:53:22 -0500

    feat: update prelude to include additional utility functions and modules

--- src-tauri/src/modules/inference_models/prelude.rs
+++ src-tauri/src/modules/inference_models/prelude.rs
@@ -1,2 +1,5 @@
-pub use super::get_inference_model_status;
-pub use super::parser::{filter_data_table, html_table_to_df};
+pub use super::dto::InferenceModelStatusRowData;
+pub use super::fetch::fetch_data;
+pub use super::parser::html_table_to_df;
+pub use super::query::query_models;
+pub use super::utils::{filter_data_table, parse_number, parse_price};
