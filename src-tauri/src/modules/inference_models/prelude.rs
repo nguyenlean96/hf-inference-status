e54b5a9f0f0dc686a8eab60e554827647b236047
Le An Nguyen <nguyenlean96@gmail.com>
2026-01-14 20:29:03 -0500

    feat: rename filter_data_table to select_html_data_table for clarity

--- src-tauri/src/modules/inference_models/prelude.rs
+++ src-tauri/src/modules/inference_models/prelude.rs
@@ -2,4 +2,4 @@ pub use super::dto::{InferenceModelStatusResponse, InferenceModelStatusRowData};
 pub use super::fetch::fetch_data;
 pub use super::parser::html_table_to_df;
 pub use super::query::query_models;
-pub use super::utils::{filter_data_table, parse_number, parse_price};
+pub use super::utils::{parse_number, parse_price, select_html_data_table};
--- src-tauri/src/modules/inference_models/utils.rs
+++ src-tauri/src/modules/inference_models/utils.rs
@@ -1,6 +1,6 @@
 use scraper::{ElementRef, Html, Selector};
 
-pub fn filter_data_table(html_str: String) -> Option<String> {
+pub fn select_html_data_table(html_str: String) -> Option<String> {
     let document = Html::parse_document(&html_str);
     let table_selector =
         Selector::parse("body > div:nth-child(1) > main > div > section > div > div:nth-child(2) > div > div:nth-child(2) > div > table")
