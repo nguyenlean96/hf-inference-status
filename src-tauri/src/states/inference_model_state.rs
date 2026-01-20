39f4732778487905de4c020381c354ef1be7b26f
Le An Nguyen <nguyenlean96@gmail.com>
2026-01-20 03:02:49 -0500

    feat: add get_model_inference_service_by_id method to retrieve model inference data by ID

--- src-tauri/src/states/inference_model_state.rs
+++ src-tauri/src/states/inference_model_state.rs
@@ -2,9 +2,8 @@ use chrono::{Local, NaiveDateTime};
 use polars::prelude::*;
 use tokio::sync::Mutex;
 
-use crate::modules::inference_models::{
-    fetch::fetch_data, parser::html_table_to_df, utils::select_html_data_table,
-};
+use crate::models::hf_model_inference::HFModelInferenceStatusRowData;
+use crate::modules::inference_models::prelude::*;
 
 #[derive(Debug, Default)]
 pub struct InferenceModelStateInner {
@@ -18,17 +17,36 @@ impl InferenceModelStateInner {
         Self::default()
     }
 
+    pub fn get_model_inference_service_by_id(
+        &self,
+        id: &str,
+    ) -> Option<HFModelInferenceStatusRowData> {
+        let df = self.data.as_ref()?;
+        // filter the df id column using the given id
+        let filtered_df = df
+            .clone()
+            .lazy()
+            .filter(col("id").eq(lit(id)))
+            .collect()
+            .ok()?;
+        // convert the row to HFModelInferenceStatusRowData
+        InferenceModelStatusCollection::from(&filtered_df)
+            .data
+            .first()
+            .cloned()
+    }
+
     pub async fn update(&mut self) {
         self.is_loading = true;
 
-        if let Ok(response) = fetch_data().await {
-            if let Some(data_table) = select_html_data_table(response) {
-                if let Ok(df) = html_table_to_df(data_table) {
-                    self.data = Some(df);
-                    self.updated_at = Some(Local::now().naive_local());
-                }
-            }
+        if let Ok(response) = fetch_data().await
+            && let Some(data_table) = select_html_data_table(response)
+            && let Ok(df) = html_table_to_df(data_table)
+        {
+            self.data = Some(df);
+            self.updated_at = Some(Local::now().naive_local());
         }
+
         self.is_loading = false;
     }
 }
