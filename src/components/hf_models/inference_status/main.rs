9de0fb5603cc011202f46e4de2cb242ac278e90d
Le An Nguyen <nguyenlean96@gmail.com>
2026-01-20 03:07:28 -0500

    feat: update ModelInferenceStatus to integrate favorite inference filtering

--- src/components/hf_models/inference_status/main.rs
+++ src/components/hf_models/inference_status/main.rs
@@ -4,61 +4,33 @@ use leptos::task::spawn_local;
 use reactive_stores::Store;
 
 use super::list::ModelInferenceStatusList;
+use super::tool_bar::toggle_show_favorite_only::ToggleShowFavoriteOnly;
 use super::tool_bar::{InferenceStatusToolBar, ToolBarAction};
-use crate::states::model_inference_state::{
-    ModelInferenceServiceState, ModelInferenceServiceStateExt,
-};
+use crate::states::prelude::*;
 
 #[component]
 pub fn ModelInferenceStatus() -> impl IntoView {
     let model_inference_state = expect_context::<Store<ModelInferenceServiceState>>();
+    let favorite_inference_service_state = expect_context::<Store<FavoriteInferenceServiceState>>();
+    let is_showing_favorite_only =
+        Memo::new(move |_| favorite_inference_service_state.show_favorite_only().get());
 
     let is_loading = Memo::new(move |_| model_inference_state.is_loading());
     let request_fetch_data = move |e: MouseEvent| {
         e.prevent_default();
 
         spawn_local(async move {
-            model_inference_state.get_data().await;
+            model_inference_state
+                .get_data(is_showing_favorite_only.get())
+                .await;
         });
     };
+
     view! {
         <div class="flex flex-col max-h-full min-h-full">
             <InferenceStatusToolBar>
                 <ToolBarAction slot:tool_bar_actions_slot>
-                    <button
-                        class="p-1.5 flex items-center gap-0.5 \
-                        text-yellow-600 \
-                        bg-yellow-500/20 hover:bg-yellow-500/40 \
-                        border border-yellow-800 rounded-lg"
-                    >
-                        <svg xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="currentColor"
-                            stroke-width="2"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            class="size-4">
-                            <path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z"/>
-                        </svg>
-                        <span class="block leading-none text-sm">"Favorite"</span>
-                    </button>
-                </ToolBarAction>
-                <ToolBarAction slot:tool_bar_actions_slot>
-                    <button
-                        class="p-1.5 flex items-center gap-0.5 \
-                        text-[#aaa] hover:text-white 
-                        hover:bg-gray-900 \
-                        border border-gray-800 rounded-lg"
-                    >
-                        <svg xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 24 24" fill="none"
-                            stroke="currentColor" stroke-width="2"
-                            stroke-linecap="round" stroke-linejoin="round"
-                            class="size-4.5">
-                            <path d="M10 20a1 1 0 0 0 .553.895l2 1A1 1 0 0 0 14 21v-7a2 2 0 0 1 .517-1.341L21.74 4.67A1 1 0 0 0 21 3H3a1 1 0 0 0-.742 1.67l7.225 7.989A2 2 0 0 1 10 14z"/>
-                        </svg>
-                    </button>
+                    <ToggleShowFavoriteOnly />
                 </ToolBarAction>
                 <ToolBarAction slot:tool_bar_actions_slot>
                     <button
