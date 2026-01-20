0237615c19db0c4475fe5193d0903ccbaef1d100
Le An Nguyen <nguyenlean96@gmail.com>
2026-01-20 03:03:32 -0500

    feat: add favorite_model_inference module and update prelude for favorite model functions

--- src-tauri/src/commands/prelude.rs
+++ src-tauri/src/commands/prelude.rs
@@ -1,1 +1,4 @@
+pub use super::favorite_model_inference::{
+    add_favorite, get_favorite_ids, get_favorite_model_inference_data, is_favorite, remove_favorite,
+};
 pub use super::inference_model_status::get_data;
