8a1cb11553314fa89946e8273086c4b1309d8374
Le An Nguyen <nguyenlean96@gmail.com>
2026-01-20 03:02:33 -0500

    feat: add UserFavoriteState for managing user favorite models with async support

---
+++ src-tauri/src/states/user_favorite_state.rs
@@ -0,0 +1,37 @@
+use tokio::sync::Mutex;
+
+#[derive(Debug, Default)]
+pub struct UserFavoriteStateInner {
+    pub is_loading: bool,
+    pub fav_ids: Vec<String>,
+}
+
+impl UserFavoriteStateInner {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn load_saved_favorites(&mut self, fav_ids: Vec<String>) {
+        self.fav_ids = fav_ids;
+    }
+
+    pub fn add_favorite(&mut self, id: &str) {
+        self.fav_ids.push(id.to_string());
+        println!("Adding: {}\nCurrent favorites: {:?}", id, self.fav_ids);
+    }
+
+    pub fn is_favorite(&self, id: &str) -> bool {
+        self.fav_ids.contains(&id.to_string())
+    }
+
+    pub fn remove_favorite(&mut self, id: &str) {
+        self.fav_ids.retain(|x| x != id);
+        println!("Removing: {}\nCurrent favorites: {:?}", id, self.fav_ids);
+    }
+
+    pub async fn get_favorite_ids(&self) -> Vec<String> {
+        self.fav_ids.clone()
+    }
+}
+
+pub type UserFavoriteState = Mutex<UserFavoriteStateInner>;
